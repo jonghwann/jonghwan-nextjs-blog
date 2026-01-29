4d64c2c98085e201dd425a9e240c71db61d22837
jonghwann <jonghwan0729@gmail.com>
2026-01-30 02:37:28 +0900

    style: 포스트 목록 구분선/여백 스타일 조정

--- src/widgets/post-list/ui/post-list.tsx
+++ src/widgets/post-list/ui/post-list.tsx
@@ -9,7 +9,7 @@ export function PostList({ posts, className }: PostListProps) {
   return (
     <ul className={className}>
       {posts.map((post) => (
-        <li key={post.slug} className="mb-8 border-b pb-12 last:mb-0 last:border-none last:pb-0">
+        <li key={post.slug} className="mb-12 border-b pb-12 last:mb-0 last:border-none last:pb-0">
           <PostItem {...post} />
         </li>
       ))}
